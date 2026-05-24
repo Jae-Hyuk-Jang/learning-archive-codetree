f6f660686f08ff0d788b9c91551df109c7791962
Jae-Hyuk-Jang <116997523+Jae-Hyuk-Jang@users.noreply.github.com>
2026-05-24 11:03:50 +0900

    [Passed] 최대 증가 부분 수열 (3ms, 0MB)

---
+++ trail4/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define MX 1010
+
+using namespace std;
+
+int N;
+int arr[MX];
+int dp[MX];
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    for (int i = 1; i<= N; i++) cin >> arr[i];
+    int answer = 1;
+    for (int i = 1; i <= N; i++) {
+        dp[i] = 1;
+        for(int j =1; j < i; j++) {
+            if(arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] +1);
+            }
+        }
+        answer = max(answer, dp[i]);
+    }
+    cout << answer;
+    return 0;
+}
