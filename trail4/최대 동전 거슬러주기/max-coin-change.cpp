8811b49f5d635f47443b6a48a05bda45891dbb90
Jae-Hyuk-Jang <116997523+Jae-Hyuk-Jang@users.noreply.github.com>
2026-06-03 20:25:16 +0900

    [Passed] 최대 동전 거슬러주기 (2ms, 0MB)

---
+++ trail4/최대 동전 거슬러주기/max-coin-change.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define MX 10010
+
+using namespace std;
+
+int N, M;
+int coin[105];
+int dp[MX];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) cin >> coin[i];
+    for (int i = 0; i <= M; i++) dp[i] = -1;
+
+    dp[0] = 0;
+
+    for (int money = 1; money <= M; money++) {
+        for (int i = 1; i <= N; i++) {
+            if (money - coin[i] < 0) continue;
+            if (dp[money - coin[i]] == -1) continue;
+            dp[money] = max(dp[money], dp[money - coin[i]] + 1);
+        }
+    }
+    cout << dp[M];
+    return 0;
+}
