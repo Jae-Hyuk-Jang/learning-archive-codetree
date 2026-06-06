b1cebc9db312b4badd36471ba8a486d67ece1c19
Jae-Hyuk-Jang <116997523+Jae-Hyuk-Jang@users.noreply.github.com>
2026-06-06 11:26:50 +0900

    [Passed] 배낭 채우기 2 (2ms, 0MB)

---
+++ trail4/배낭 채우기 2/knapsack-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+#define MX 10010
+
+using namespace std;
+
+int N, M;
+int w[105], v[105];
+int dp[MX];
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) cin >> w[i] >> v[i];
+    
+    for (int i = 1; i <= N; i++) {
+        for (int weight = w[i]; weight <= M; weight++) {
+            dp[weight] = max(dp[weight], dp[weight - w[i]] + v[i]);
+        }
+    }
+    
+    cout << dp[M];
+    return 0;
+}
