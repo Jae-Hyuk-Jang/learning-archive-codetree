e5533062640c00fa6e2adcb43610ab5f409d31b2
Jae-Hyuk-Jang <116997523+Jae-Hyuk-Jang@users.noreply.github.com>
2026-06-23 11:06:52 +0900

    [Passed] 트리 완성 (22ms, 1MB)

---
+++ trail6/트리 완성/tree-completion.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+#define MX 100010
+
+using namespace std;
+
+int N, M;
+int parent[MX];
+
+int Find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = Find(parent[x]);
+}
+
+void Union(int a, int b) {
+    a = Find(a);
+    b = Find(b);
+
+    if (a == b) return;
+
+    if (a < b) parent[b] = a;
+    else parent[a] = b;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) parent[i] = i;
+
+    for (int i = 0; i < M; i++) {
+        int a, b; cin >> a >> b;
+        Union(a, b);
+    }
+
+    int component = 0;
+
+    for (int i = 1; i <= N; i++) {
+        if (Find(i) == i) component++;
+    }
+
+    int removeCnt = M - (N - component);
+    int addCnt = component - 1;
+
+    cout << removeCnt + addCnt << '\n';
+
+    return 0;
+}
