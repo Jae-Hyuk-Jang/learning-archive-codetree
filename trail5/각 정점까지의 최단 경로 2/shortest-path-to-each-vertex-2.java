80e11cfa3fe122a966a99ab126b3430fdaf2a3bc
Jae-Hyuk-Jang <116997523+Jae-Hyuk-Jang@users.noreply.github.com>
2026-07-28 13:11:54 +0900

    [Passed] 각 정점까지의 최단 경로 2 (1142ms, 52MB)

---
+++ trail5/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Edge {
+    int x, y, z;
+    
+    public Edge(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+};
+
+public class Main {    
+    public static final int MAX_N = 100;
+    
+    // 변수 선언
+    public static int[][] dist = new int[MAX_N + 1][MAX_N + 1];
+
+    public static int n, m;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++)
+                dist[i][j] = (int)1e9;
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a, b, c;
+            a = sc.nextInt();
+            b = sc.nextInt();
+            c = sc.nextInt();
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+        
+        for(int k = 1; k <= n; k++) 
+            for(int i = 1; i <= n; i++)
+                for(int j = 1; j <= n; j++)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(dist[i][j] == (int)1e9)
+                    System.out.print(-1 + " ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
